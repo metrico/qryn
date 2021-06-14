808b01bb7dde07bbad089a9f82ecea5c4236a692
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-06-15 00:22:54 +0200

    Update cloki.js

--- cloki.js
+++ cloki.js
@@ -49,9 +49,6 @@ fastify.register(require("fastify-url-data"), (err) => {
     if (err) throw err;
 });
 
-const handler_404 = require('./lib/handlers/404.js').bind(this);
-fastify.setNotFoundHandler(handler_404);
-
 /* Enable Simple Authentication */
 if (this.http_ && this.http_password) {
     fastify.register(require("fastify-basic-auth"), {
@@ -61,7 +58,6 @@ if (this.http_ && this.http_password) {
         fastify.addHook("preHandler", fastify.basicAuth);
     });
 }
-
 function validate(username, password, req, reply, done) {
     if (username === this.http_user && password === this.http_password) {
         done();
@@ -82,6 +78,7 @@ fastify.addContentTypeParser("text/plain", {
     }
 });
 
+/* Protobuf Handler */
 fastify.addContentTypeParser("application/x-protobuf", function(req, done) {
     var data = "";
     req.on("data", (chunk) => {
@@ -95,24 +92,15 @@ fastify.addContentTypeParser("application/x-protobuf", function(req, done) {
     });
 });
 
+/* 404 Handler */
+const handler_404 = require('./lib/handlers/404.js').bind(this);
+fastify.setNotFoundHandler(handler_404);
+
+/* Hello cloki test API */
 const handler_hello = require('./lib/handlers/hello.js').bind(this);
 fastify.get("/hello", handler_hello);
 
 /* Write Handler */
-/*
-    Accepts JSON formatted requests when the header Content-Type: application/json is sent.
-    Example of the JSON format:
-	{
-	    "streams": [
-	        {
-	            "labels": "{foo=\"bar\"}",
-	            "entries": [
-	                {"ts": "2018-12-18T08:28:06.801064-04:00", "line": "baz"}
-	            ]
-	        }
-	    ]
-	}
-*/
 const handler_push = require('./lib/handlers/push.js').bind(this);
 fastify.post("/loki/api/v1/push", handler_push);
 
@@ -121,54 +109,20 @@ const handler_telegraf = require('./lib/handlers/telegraf.js').bind(this);
 fastify.post("/telegraf", handler_telegraf);
 
 /* Query Handler */
-/*
-   For doing queries, accepts the following parameters in the query-string:
-	query: a logQL query
-	limit: max number of entries to return
-	start: the start time for the query, as a nanosecond Unix epoch (nanoseconds since 1970)
-	end: the end time for the query, as a nanosecond Unix epoch (nanoseconds since 1970)
-	direction: forward or backward, useful when specifying a limit
-	regexp: a regex to filter the returned results, will eventually be rolled into the query language
-*/
-
 const handler_query_range = require('./lib/handlers/query_range.js').bind(this);
 fastify.get("/loki/api/v1/query_range", handler_query_range);
 
 /* Label Handlers */
-/*
-   For retrieving the names of the labels one can query on.
-   Responses looks like this:
-	{
-	  "values": [
-	    "instance",
-	    "job",
-	    ...
-	  ]
-	}
-*/
-
 /* Label Value Handler via query (test) */
 const handler_query = require('./lib/handlers/query.js').bind(this);
 fastify.get("/loki/api/v1/query", handler_query);
 
-/* Is it Label or Labels? */
+/* Label Handlers */
 const handler_label = require('./lib/handlers/label.js').bind(this);
 fastify.get("/loki/api/v1/label", handler_label);
 fastify.get("/loki/api/v1/labels", handler_label);
 
 /* Label Value Handler */
-/*
-   For retrieving the label values one can query on.
-   Responses looks like this:
-	{
-	  "values": [
-	    "default",
-	    "cortex-ops",
-	    ...
-	  ]
-	}
-*/
-
 const handler_label_values = require('./lib/handlers/label_values.js').bind(this);
 fastify.get("/loki/api/v1/label/:name/values", handler_label_values);
 
@@ -183,6 +137,6 @@ fastify.listen(
     (err, address) => {
         if (err) throw err;
         console.log("cLoki API up");
-        fastify.log.info(`server listening on ${address}`);
+        fastify.log.info(`cloki API listening on ${address}`);
     }
 );
