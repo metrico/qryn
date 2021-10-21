980ccd474ec65706e2e8ad687f5ee8f16d7823f8
Dletta <jcduschletta@gmail.com>
2021-10-21 14:04:43 -0500

    push fix to naming

--- plugins/extract_var/index.js
+++ plugins/extract_var/index.js
@@ -2,7 +2,7 @@ const {PluginLoaderBase} = require('plugnplay');
 module.exports = class extends PluginLoaderBase {
     exportSync() {
         return {
-            bnf: `MACRO_test_macro_fn ::= "extract" <OWSP> "(" <OWSP> <quoted_str1> <OWSP> "," <OWSP> <quoted_str2> <OWSP> "," <OWSP> <quoted_str3> <OWSP> "," <OWSP> <quoted_str4> <OWSP> ")"`,
+            bnf: `MACRO_extract_var_fn ::= "extract" <OWSP> "(" <OWSP> <quoted_str1> <OWSP> "," <OWSP> <quoted_str2> <OWSP> "," <OWSP> <quoted_str3> <OWSP> "," <OWSP> <quoted_str4> <OWSP> ")"`,
             /**
              *
              * @param token {Token}
