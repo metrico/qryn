21d7683fa1118084788c7c6f4fdc319a73c837ce
Dletta <jcduschletta@gmail.com>
2021-10-21 14:30:04 -0500

    change first one to label, not string

--- plugins/extract_var/index.js
+++ plugins/extract_var/index.js
@@ -2,14 +2,14 @@ const {PluginLoaderBase} = require('plugnplay');
 module.exports = class extends PluginLoaderBase {
     exportSync() {
         return {
-            bnf: `MACRO_extract_var_fn ::= "extract" <OWSP> "(" <OWSP> <quoted_str> <OWSP> "," <OWSP> <quoted_str> <OWSP> "," <OWSP> <quoted_str> <OWSP> "," <OWSP> <quoted_str> <OWSP> ")"`,
+            bnf: `MACRO_extract_var_fn ::= "extract" <OWSP> "(" <OWSP> <label> <OWSP> "," <OWSP> <quoted_str> <OWSP> "," <OWSP> <quoted_str> <OWSP> "," <OWSP> <quoted_str> <OWSP> ")"`,
             /**
              *
              * @param token {Token}
              * @returns {string}
              */
             stringify: (token) => {
-                return `first_over_time({${token.Children('quoted_str')[0].value}=${token.Children('quoted_str')[1].value}} | json | line_format "\"${token.Children('quoted_str')[2].value}\":\"{{${token.Children('quoted_str')[3].value}}}\""| unwrap _entry [5s])`;
+                return `first_over_time({${token.Child('label')[0].value}=${token.Children('quoted_str')[1].value}} | json | line_format "\"${token.Children('quoted_str')[2].value}\":\"{{${token.Children('quoted_str')[3].value}}}\""| unwrap _entry [5s])`;
             }
         };
     }
