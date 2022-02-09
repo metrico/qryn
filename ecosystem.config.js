e7a85abaee90ab4a9bdc934c0db7048a76186a20
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-02-09 23:27:16 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,6 @@ module.exports = {
     name: 'cloki',
     script: './cloki.js',
     instances : "max",
-    exec_mode : "cluster",
-    env: {
-      CLICKHOUSE_SERVER: 'localhost',
-      CLICKHOUSE_PORT: 8123,
-      CLICKHOUSE_AUTH: 'default:password',
-      CLICKHOUSE_DB: 'cloki',
-      TIMEFIELD: 'record_datetime',
-      LABELS_DAYS: 7,
-      SAMPLES_DAYS: 7,
-      DEBUG: false
-    }
+    exec_mode : "cluster"
   }]
 }
