da9764c67c9d169371f2781d534d8a008c3cf62d
akvlad <akvlad90@gmail.com>
2022-04-21 19:28:04 +0300

    cliql_v2

--- parser/cliql/transpiler.js
+++ parser/cliql/transpiler.js
@@ -1,6 +1,9 @@
 const { DATABASE_NAME } = require('../../lib/utils')
 const Sql = require('@cloki/clickhouse-sql')
 const lineFilterOperatorRegistry = require('../registry/line_filter_operator_registry')
+const logRangeAggregationRegistry = require('../registry/log_range_aggregation_registry')
+const aggregationOperatorRegistry = require('../registry/high_level_aggregation_registry')
+const unwrapRegistry = require('../registry/unwrap_registry')
 const parserRegistry = require('../registry/parser_registry')
 const { getStream } = require('../registry/common')
 
@@ -35,17 +38,20 @@ module.exports.transpile = (root, request) => {
   if (val && !root.Child('unwrap_value_statement')) {
     throw new Error('--value tag should have unwrap_value pipeline')
   }
-
+  const tsMs = val || root.Child('log_range_aggregation')
   const lbls = new Labels()
+  const tsNsExp = `toInt64(toFloat64(${tsNS}) * pow(10, 18 - toUInt8(log10(toFloat64(${tsNS})))))`
   let query = (new Sql.Select())
     .select(
-      (msg ? [new Sql.Raw(msg), 'string'] : [new Sql.Raw(val), 'value']),
+      (msg ? [new Sql.Raw(msg), 'string'] : null),
+      (val ? [new Sql.Raw(val), 'value'] : null),
       [new Sql.Raw('1'), 'fingerprint'],
-      (msg ? [new Sql.Raw(tsNS), 'timestamp_ns'] : [new Sql.Raw(`intDiv(${tsNS}, 1000000)`), 'timestamp_ns']),
+      (tsMs ? [new Sql.Raw(`intDiv(${tsNsExp}, 1000000)`), 'timestamp_ns'] : [new Sql.Raw(tsNsExp), 'timestamp_ns']),
       [lbls, 'labels']
     ).from([`${db}.${table}`, 'samples'])
     .where(Sql.And(Sql.Lt('samples.timestamp_ns', request.end), Sql.Gte('samples.timestamp_ns', request.start)))
     .limit(msg ? parseInt(request.limit) : 0)
+  query.select_list = query.select_list.filter(s => s)
   if (!val) {
     query.orderBy(['timestamp_ns', 'desc'])
   } else {
@@ -56,13 +62,11 @@ module.exports.transpile = (root, request) => {
       const op = pipeline.Child('line_filter_operator').value
       query = lineFilterOperatorRegistry[op](pipeline, query)
       continue
-    }
-    if (pipeline.Child('parser_expression')) {
+    } else if (pipeline.Child('parser_expression')) {
       const op = pipeline.Child('parser_fn_name').value
       query = parserRegistry[op](pipeline, query)
       continue
-    }
-    if (pipeline.Child('labels_format_expression')) {
+    } else if (pipeline.Child('labels_format_expression')) {
       for (const param of pipeline.Children('labels_format_expression_param')) {
         if (param.Child('label_inject_param')) {
           lbls.addLbl(param.Child('label').value, JSON.parse(param.Child('quoted_str').value))
@@ -72,14 +76,96 @@ module.exports.transpile = (root, request) => {
           lbls.addLbl(_lbls[0].value, _lbls[1].value)
         }
       }
+    } else if (pipeline.Child('label_filter_pipeline')) {
+      let where = ''
+      const marshal = (token) => {
+        switch (token.name) {
+          case 'label_filter_pipeline':
+          case 'complex_label_filter_expression':
+            token.tokens.forEach(marshal)
+            break
+          case 'bracketed_label_filter_expression':
+            where += '('
+            token.tokens.forEach(marshal)
+            where += ')'
+            break
+          case 'and_or':
+            where += ' ' + token.value + ' '
+            break
+          case 'label_filter_expression':
+            token.tokens.forEach(marshal)
+            break
+          case 'string_label_filter_expression':
+          case 'number_label_filter_expression':
+            // eslint-disable-next-line no-case-declarations
+            const lbl = token.Child('label').value
+            // eslint-disable-next-line no-case-declarations
+            const expr = lbls.getLbl(lbl)
+            // eslint-disable-next-line no-case-declarations
+            const val = token.name === 'number_label_filter_expression'
+              ? token.Child('NUMBER').value
+              : JSON.parse(token.Child('quoted_str').value)
+            // eslint-disable-next-line no-case-declarations
+            let op = token.Child(token.name === 'number_label_filter_expression'
+              ? 'number_operator'
+              : 'operator').value
+            op = token.name === 'number_label_filter_expression' && op === '!=' ? '!==' : op
+            if (expr) {
+              where += labelFilterOps[op](expr, val)
+            } else {
+              if (query.select_list.some(f => f[1] === 'extra_labels')) {
+                where += '(' +
+                  `arrayExists(x -> x.1 == '${lbl}') AND ` +
+                  labelFilterOps[lbl.Child('operator')](`arrayFirst(x -> x.1 == ${lbl}).2`, val) + ')'
+              }
+            }
+        }
+      }
+      marshal(pipeline.Child('label_filter_pipeline'))
+      const cond = new Sql.Condition()
+      cond.toString = () => `(${where})`
+      query.where(cond)
+    } else {
+      throw new Error('Not supported')
     }
+  }
+  let matrix = !!val
+  let duration = 1
+  query.ctx.step = request.step
+  if (root.Child('log_range_aggregation')) {
+    const op = root.Child('log_range_aggregation_fn').value
+    if (!logRangeAggregationRegistry[op]) {
+      throw new Error('Not supported')
+    }
+    query = logRangeAggregationRegistry[op](root.Child('log_range_aggregation'), query)
+    matrix = true
+    duration = query.ctx.duration
+  }
+  if (root.Child('unwrap_function')) {
+    const op = root.Child('unwrap_fn').value
+    if (!unwrapRegistry[op]) {
+      throw new Error('Not supported')
+    }
+    query.select_list = query.select_list.map(l => l[1] === 'value' ? [l[0], 'unwrapped'] : l)
+    query = unwrapRegistry[op](root.Child('unwrap_function'), query)
+    matrix = true
+    duration = query.ctx.duration
+  }
+  if (root.Child('aggregation_operator')) {
+    const op = root.Child('aggregation_operator_fn').value
+    if (!aggregationOperatorRegistry[op]) {
+      throw new Error('Not supported')
+    }
+    query = aggregationOperatorRegistry[op](root.Child('aggregation_operator'), query)
+    matrix = true
+    duration = query.ctx.duration
   }
   console.log(query.toString())
   return {
     query: request.rawQuery ? query : query.toString(),
     stream: getStream(query),
-    matrix: !!val,
-    duration: 1
+    matrix: matrix,
+    duration: duration
   }
 }
 
@@ -106,10 +192,48 @@ class Labels extends Sql.Raw {
   }
 
   addLbl (name, sql) {
-    this.labels.push([Sql.val(name), new Sql.Raw(sql)])
+    this.labels.push([name, sql])
+  }
+
+  getLbl (name) {
+    return (this.labels.find(l => l[0] === name) || [undefined, undefined])[1]
   }
 
   toString () {
-    return '[' + this.labels.map(l => `(${l[0]}, ${l[1]})`).join(',') + ']'
+    return '[' + this.labels.filter(l => l[0].substr(0, 2) !== '--')
+      .map(l => `(${Sql.val(l[0])}, ${l[1]})`).join(',') + ']::Array(Tuple(String, String))'
+  }
+}
+
+const labelFilterOps = {
+  '=': (lbl, val) => {
+    return `position(${lbl}, ${val}) != 0`
+  },
+  '!=': (lbl, val) => {
+    return `position(${lbl}, ${val}) == 0`
+  },
+  '=~': (lbl, val) => {
+    return `match(${lbl}, ${val}) == 1`
+  },
+  '!~': (lbl, val) => {
+    return `match(${lbl}, ${val}) == 0`
+  },
+  '==': (lbl, val) => {
+    return `${lbl} == ${val}`
+  },
+  '!==': (lbl, val) => {
+    return `${lbl} != ${val}`
+  },
+  '>': (lbl, val) => {
+    return `${lbl} > ${val}`
+  },
+  '>=': (lbl, val) => {
+    return `${lbl} >= ${val}`
+  },
+  '<': (lbl, val) => {
+    return `${lbl} < ${val}`
+  },
+  '<=': (lbl, val) => {
+    return `${lbl} <= ${val}`
   }
 }
