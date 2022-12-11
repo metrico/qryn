41a3570a85896ea3dded4511f9e8c5980da9662e
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-12-11 13:36:25 +0100

    Create index.js
    Signed-off-by: Lorenzo Mangani <lorenzo.mangani@gmail.com>

---
+++ plugins/mongo_filter/index.js
@@ -0,0 +1,59 @@
+const { PluginLoaderBase } = require('plugnplay')
+const { compileMongoQuery } = require('mongo-query-compiler')
+
+/**
+
+Mongodb-like JSON filtering output processor.
+Usage: mongo({age: {$exists: true}}, {type="people"}
+
+**/
+
+/**
+ * @class Plugin
+ * @property {string} query
+ * @property start {number} start in NS
+ * @property end {string} end in NS
+ * @property type {string} promql or logql
+ * @property limit {number}
+ * @property {{
+ *   logql: (query: string, startNS: number, endNS: number, limit: number) => Promise<Object>
+ * }} API
+ *   promql: (query: string, startNS: number, endNS: number, limit: number) => Promise<Object> //not implemented
+ */
+class Plugin {
+  /**
+   * @method
+   * @name check
+   * @this {Plg}
+   * @returns {boolean} if this plugin is usable for the query
+   */
+  check () {
+    return this.query.match(/^mongo\(.+\)\s*$/)
+  }
+
+  /**
+   * @method
+   * @name process
+   * @this {Plg}
+   * @returns {Promise<{type: string, out: string}>} The raw output
+   */
+  async process () {
+    const match = this.query.match(/^mongo\({(.+)}, (.+)\)$/)
+    const response = await this.API.logql(match[2], this.start, this.end, this.limit)
+    // Filter using Mongo Query Compiler
+    let query = match[2];
+    let filterer = compileMongoQuery(query);
+    let results = response.data.result.filter(filterer);
+    return {
+      type: 'application/json',
+      out: results
+    }
+  }
+}
+class Plg extends PluginLoaderBase {
+  exportSync (api) {
+    return new Plugin()
+  }
+}
+
+module.exports = Plg
