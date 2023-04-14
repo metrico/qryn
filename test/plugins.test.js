99847620bea9e6acb1cfc15de025e5b36484a8ec
akvlad <akvlad90@gmail.com>
2023-04-14 15:10:32 +0300

    cyrllic support init

--- test/plugins.test.js
+++ test/plugins.test.js
@@ -3,3 +3,7 @@ const { getPlg } = require('../plugins/engine')
 it('should glob', () => {
   expect(getPlg({ type: 'unwrap_registry' })).toBeTruthy()
 })
+
+it('should unicode chars', () => {
+  console.log('АąŚĄ'.match(/\p{L}/ug))
+})
