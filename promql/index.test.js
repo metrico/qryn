ab59344462c8cacd51365e0d29d86f73ce24b096
akvlad <akvlad90@gmail.com>
2023-10-16 14:12:38 +0300

    #fix: rm outdated unit tests

--- promql/index.test.js
+++
@@ -1,7 +0,0 @@
-const { request } = require('./index');
-
-(async () => {
-  await new Promise((resolve => setTimeout(resolve, 1000)));
-  const res = await request('test{}', Date.now() - 300000, Date.now(), 15000)
-  console.log(res)
-})()
