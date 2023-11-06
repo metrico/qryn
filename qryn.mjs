f6114976ba4751f50330195999b0095172693ad0
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-06 16:26:26 +0100

    Update qryn.mjs
    Signed-off-by: Lorenzo Mangani <lorenzo.mangani@gmail.com>

--- qryn.mjs
+++ qryn.mjs
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+/*
+ * qryn: polyglot observability API
+ * (C) 2018-2024 QXIP BV
+ */
+
 import {init} from './qryn_node_wrapper.js'
 import {bun} from './common.js'
 import bunInit from './qryn_bun.mjs'
