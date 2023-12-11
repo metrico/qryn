73078524c8738d7db27344a99b9568f062782adc
metrico <lorenzo.mangani@gmail.com>
2023-12-11 17:05:00 +0000

    Upgrade view to v0.27.4

--- view/assets/reactDnd-dc8b0776.js
+++ view/assets/reactDnd-dc8b0776.js
@@ -1,4 +1,4 @@
-import{r as h,g as He}from"./react-432945ee.js";import{_ as je}from"./vendor-6029fc34.js";var pe={exports:{}},R={};/**
+import{r as h,g as He}from"./react-432945ee.js";import{_ as je}from"./vendor-3db6068a.js";var pe={exports:{}},R={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
