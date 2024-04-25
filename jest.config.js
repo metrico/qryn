c618b37fafccaf3c0320f97eae7b68c0c7ee480c
akvlad <akvlad90@gmail.com>
2024-04-25 14:08:13 +0300

    fix: axios update

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
 module.exports = {
-  setupFilesAfterEnv: [path.join(__dirname, '/test/jest.setup.js')]
+  setupFilesAfterEnv: [path.join(__dirname, '/test/jest.setup.js')],
+  moduleNameMapper: {
+    '^axios$': 'axios/dist/node/axios.cjs'
+  }
 }
