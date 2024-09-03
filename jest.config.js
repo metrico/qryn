422c99225e37132c0ad197d6cb2d2b813fcecba5
akvlad <akvlad90@gmail.com>
2024-09-03 15:28:08 +0300

    fix security alerts

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ const path = require('path')
 module.exports = {
   setupFilesAfterEnv: [path.join(__dirname, '/test/jest.setup.js')],
   moduleNameMapper: {
-    '^axios$': 'axios/dist/node/axios.cjs'
+    '^axios$': require.resolve('axios'),//'axios/dist/node/axios.cjs'
   }
 }
